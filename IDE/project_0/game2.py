c9e5b1bae18a19786cb75077f375e48f9347689c
IksLega <dyakivnich.vladimir@gmail.com>
2023-11-12 16:13:09 +0500

    edit game2.py

--- IDE/project_0/game2.py
+++ IDE/project_0/game2.py
@@ -95,11 +95,11 @@ def game_core_v3(number: int = 1) -> int:
 
 
 #Run benchmarking to score effectiveness of all algorithms
-print('Run benchmarking for random_predict: ', end='')
-score_game(random_predict)
+#print('Run benchmarking for random_predict: ', end='')
+#score_game(random_predict)
 
-print('Run benchmarking for game_core_v2: ', end='')
-score_game(game_core_v2)
+#print('Run benchmarking for game_core_v2: ', end='')
+#score_game(game_core_v2)
 
-print('Run benchmarking for game_core_v3: ', end='')
-score_game(game_core_v3)
+#print('Run benchmarking for game_core_v3: ', end='')
+#score_game(game_core_v3)
